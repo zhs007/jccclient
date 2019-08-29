7c06cf4597864958560d71f474489d662beed0d9
zhs007 <sssxueren@gmail.com>
2019-08-29 16:47:25 +0800

    - updated proto

--- basedef.go
+++ basedef.go
@@ -1,7 +1,7 @@
 package jccclient
 
 // VERSION - jccclient version
-const VERSION = "0.1.21"
+const VERSION = "0.1.22"
 
 // MaxMultiFailTimes - If the client fail {{MaxMultiFailTimes}} times in succession, it need to ignore some task
 const MaxMultiFailTimes = 3
