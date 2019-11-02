ec4e7a2907e8f1343070d3f363fdcd7784697a73
zhs007 <sssxueren@gmail.com>
2019-11-02 21:00:35 +0800

    - updated proto

--- buildproto.sh
+++ buildproto.sh
@@ -1,2 +1,3 @@
+protoc -I proto/ proto/alimama.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/result.proto --go_out=plugins=grpc:proto
 protoc -I dbpb/ dbpb/db.proto --go_out=plugins=grpc:dbpb
