562c0f81ae97b92ebb436fca20ac61134dc10847
zhs007 <sssxueren@gmail.com>
2019-11-14 17:16:40 +0800

    - updated proto

--- buildproto.sh
+++ buildproto.sh
@@ -2,5 +2,6 @@ protoc -I proto/ proto/alimama.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/steepandcheap.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/mountainsteals.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/tmall.proto --go_out=plugins=grpc:proto
+protoc -I proto/ proto/taobao.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/result.proto --go_out=plugins=grpc:proto
 protoc -I dbpb/ dbpb/db.proto --go_out=plugins=grpc:dbpb
---
+++ updproto.sh
@@ -0,0 +1,1 @@
+cp -rf ../jarviscrawlercore/proto/*.proto ./proto/
