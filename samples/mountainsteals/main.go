472e4b66759a6cc2ded43898c704fea77c0d41fd
zhs007 <sssxueren@gmail.com>
2019-11-25 16:36:24 +0800

    - updated mountainsteals

--- samples/mountainsteals/main.go
+++ samples/mountainsteals/main.go
@@ -10,41 +10,33 @@ import (
 func main() {
 	client := jccclient.NewClient(nil, "127.0.0.1:7051", "wzDkh9h2fhfUVuS9jZ8uVbhV3vC5AWX3")
 
-	// err := client.AlimamaKeepalive(context.Background(),
+	// reply1, err := client.MountainStealsSale(context.Background(),
+	// 	"promo/msbf19",
 	// 	3*60*1000)
 
 	// if err != nil {
-	// 	fmt.Printf("AlimamaKeepalive %v", err)
+	// 	fmt.Printf("MountainStealsSale %v", err)
 
 	// 	return
 	// }
 
-	reply1, err := client.MountainStealsSale(context.Background(),
-		"531445101234",
+	// if reply1 != nil {
+	// 	fmt.Printf("\n%v", reply1)
+	// }
+
+	reply2, err := client.MountainStealsProduct(context.Background(),
+		"canada-goose-men-s-waffle-slouchy-beanie_10360288",
 		3*60*1000)
 
 	if err != nil {
-		fmt.Printf("MountainStealsSale %v", err)
+		fmt.Printf("MountainStealsProduct %v", err)
 
 		return
 	}
 
-	if reply1 != nil {
-		fmt.Printf("\n%v", reply1)
+	if reply2 != nil {
+		fmt.Printf("\n%v", reply2)
 	}
-
-	// reply2, err := client.AlimamaGetTop(context.Background(),
-	// 	3*60*1000)
-
-	// if err != nil {
-	// 	fmt.Printf("AlimamaGetTop %v", err)
-
-	// 	return
-	// }
-
-	// if reply2 != nil {
-	// 	fmt.Printf("\n%v", reply2)
-	// }
 
 	return
 }
