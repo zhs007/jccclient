c62e520941c6e73510f373d17dcacd6a1a80a038
zhs007 <sssxueren@gmail.com>
2019-12-20 14:30:12 +0800

    - updated proto

--- basedef.go
+++ basedef.go
@@ -1,7 +1,7 @@
 package jccclient
 
 // VERSION - jccclient version
-const VERSION = "0.3.3"
+const VERSION = "0.3.5"
 
 // MaxMultiFailTimes - If the client fail {{MaxMultiFailTimes}} times in succession, it need to ignore some task
 const MaxMultiFailTimes = 3
--- buildproto.sh
+++ buildproto.sh
@@ -1,3 +1,4 @@
+protoc -I proto/ proto/searchparam2.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/alimama.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/steepandcheap.proto --go_out=plugins=grpc:proto
 protoc -I proto/ proto/mountainsteals.proto --go_out=plugins=grpc:proto
