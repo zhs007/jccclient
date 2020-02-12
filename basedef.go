7108b5de7b66eeb93c53fd2ce6437c20c394c1ee
zerro <zerrozhao@gmail.com>
2020-02-12 16:41:32 +0800

    - Added Tag.Or

--- basedef.go
+++ basedef.go
@@ -1,7 +1,7 @@
 package jccclient
 
 // VERSION - jccclient version
-const VERSION = "0.3.12"
+const VERSION = "0.3.13"
 
 // MaxMultiFailTimes - If the client fail {{MaxMultiFailTimes}} times in succession, it need to ignore some task
 const MaxMultiFailTimes = 3
