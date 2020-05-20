5f1e80740fca65b71aa9046f7b93776a370ab98d
zhs007 <sssxueren@gmail.com>
2020-05-20 19:04:18 +0800

    - Removed log for ``Can't find client``

--- basedef.go
+++ basedef.go
@@ -1,7 +1,7 @@
 package jccclient
 
 // VERSION - jccclient version
-const VERSION = "0.5.10"
+const VERSION = "0.5.11"
 
 // MaxMultiFailTimes - If the client fail {{MaxMultiFailTimes}} times in succession, it need to ignore some task
 const MaxMultiFailTimes = 3
